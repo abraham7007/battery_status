271393e2a776a719ca44a26577eb7a4d39140e70
abraham <acasot@uni.pe>
2020-09-07 17:30:32 -0500

    se hizo una prueba para cada codigo 1 y 2

--- batteryStatus/batteryStatus.ino
+++ batteryStatus/batteryStatus.ino
@@ -7,9 +7,12 @@ void setup(){
 
 void loop(){
     int sensorValue = analogRead(A0);
-    float voltaje = sensorValue*(5/1023.00)*2.70;
-    Serial.println(voltaje);
-    if (voltaje<9){
+    float voltaje = sensorValue*(5/1023.00)*0.93;
+    float porcentaje = 25*voltaje;
+    Serial.print(voltaje);
+    Serial.print("  ");
+    Serial.println(porcentaje);
+    if (voltaje<2.5){
         digitalWrite(led, HIGH);
     }
     else
--- batteryStatus2/batteryStatus2.ino
+++ batteryStatus2/batteryStatus2.ino
@@ -1,45 +1,18 @@
-#define LEDGREEN 2
-#define LEDYELLOW 3
-#define LEDRED 4
 #define ANALOGPILA 0
 // VARIABLES
 int analogValor = 0;
 float voltaje = 0;
 int ledDelay = 800;
 // UMBRALES
-float maximo = 1.5;
-float maximo = 1.3;
-float maximo = 0.3;
+float maximo = 4.2;
+float medio = 3.7;
+float minimo = 2.75;
 void setup(){
   Serial.begin(9600);
-  pinMode(LEDGREEN, OUTPUT);
-  pinMode(LEDYELLOW, OUTPUT);
-  pinMode(LEDRED, OUTPUT); 
 }
 void loop(){
-  analogValor = analogRead();
+  analogValor = analogRead(A0);
   voltaje = analogValor*0.0048;
   Serial.print("voltaje");
   Serial.println(voltaje);
-  if(voltaje >= maximo){
-    digitalWrite(LEDGREEN, HIGH);  
-    delay(ledDelay);
-    digitalWrite(LEDGREEN, LOW);  
-  }
-  else if (voltaje < maximo && voltaje > medio){
-    digitalWrite(LEDYELLOW,HIGH);
-    delay(ledDelay);
-    digitalWrite(LEDYELLOW,LOW);
-  }
-  else if (voltaje < medio && voltaje > minimo){
-    digitalWrite(LEDRED,HIGH);
-    delay(ledDelay);
-    digitalWrite(LEDRED,LOW);    
-  }
-  digitalWrite(LEDGREEN, LOW);
-  digitalWrite(LEDYELOOW, LOW);
-  digitalWrite(LEDRED, LOW);
-
-
 }
-
